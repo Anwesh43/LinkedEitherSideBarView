4bf76cde53b5c2db506236382a9fbf1cc46e6123
Anwesh43 <anweshthecool0@gmail.com>
2020-08-05 02:35:51 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedeithersidebarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedeithersidebarview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedeithersidebarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.eithersidebarview.EitherSideBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EitherSideBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
