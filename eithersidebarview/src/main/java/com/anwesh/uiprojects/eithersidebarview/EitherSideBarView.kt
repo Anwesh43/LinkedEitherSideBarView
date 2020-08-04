e462a35db1120197f5737ed8c24f3904b454fe99
Anwesh43 <anweshthecool0@gmail.com>
2020-08-05 02:17:59 +0530

    adding ESBNode class

--- eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
+++ eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
@@ -120,4 +120,45 @@ class EitherSideBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ESBNode(var i : Int, val state : State = State()) {
+
+        private var next : ESBNode? = null
+        private var prev : ESBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ESBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawESBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ESBNode {
+            var curr : ESBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
