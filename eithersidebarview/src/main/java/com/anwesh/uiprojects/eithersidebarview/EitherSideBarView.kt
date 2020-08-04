e4fda70a70d927f28a5081145e5b9928fd4c8e69
Anwesh43 <anweshthecool0@gmail.com>
2020-08-05 02:10:20 +0530

    adding EitherSideBarView class

--- eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
+++ eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawESBNode(i : Int, scale : Float, paint : Paint) {
     drawEitherSideBar(scale, w, h, paint)
     restore()
 }
+
+class EitherSideBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
