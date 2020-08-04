711963f90ce4bf3514ac41419f5712dbc31acb1a
Anwesh43 <anweshthecool0@gmail.com>
2020-08-05 02:32:03 +0530

    adding renderer in view

--- eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
+++ eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
@@ -60,14 +60,16 @@ fun Canvas.drawESBNode(i : Int, scale : Float, paint : Paint) {
 
 class EitherSideBarView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
