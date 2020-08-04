453bad89e55d5f377325555af8f6350b139bd117
Anwesh43 <anweshthecool0@gmail.com>
2020-08-05 02:07:15 +0530

    adding canvas extension methods

--- eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
+++ eithersidebarview/src/main/java/com/anwesh/uiprojects/eithersidebarview/EitherSideBarView.kt
@@ -26,3 +26,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawEitherSideBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val barW : Float = Math.min(w, h) / barWidthFactor
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    rotate(90f * sf4)
+    drawRect(RectF(-size, -barW, -size + 2 * size * sf2, 0f), paint)
+    for (j in 0..1) {
+        val sfi : Float = sf.divideScale(j, parts)
+        save()
+        translate(-size + 2 * size * j, -barW)
+        drawRect(RectF(0f, -size * sfi, barW, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawESBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, h / 2)
+    drawEitherSideBar(scale, w, h, paint)
+    restore()
+}
